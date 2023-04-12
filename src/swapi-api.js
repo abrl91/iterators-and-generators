abe9993daf85862591961d2c5def031f4da96ced
Amit Barletz <amit@amplication.com>
2023-04-12 18:38:59 +0300

    swapi

---
+++ src/swapi-api.js
@@ -0,0 +1,52 @@
+import fetch from 'node-fetch';
+
+const getSwapiPageGenerator = (entity) => {
+  return async function* () {
+    let nextPage = `https://swapi.dev/api/${entity}/`;
+    while (nextPage) {
+      const response = await fetch(nextPage);
+      const data = await response.json();
+      nextPage = data.next;
+      yield data.results;
+    }
+  };
+};
+
+const swapiEntities = {
+  people: {
+    [Symbol.asyncIterator]: getSwapiPageGenerator('people'),
+  },
+  planets: {
+    [Symbol.asyncIterator]: getSwapiPageGenerator('planets'),
+  },
+  films: {
+    [Symbol.asyncIterator]: getSwapiPageGenerator('films'),
+  },
+  species: {
+    [Symbol.asyncIterator]: getSwapiPageGenerator('species'),
+  },
+  vehicles: {
+    [Symbol.asyncIterator]: getSwapiPageGenerator('vehicles'),
+  },
+  starships: {
+    [Symbol.asyncIterator]: getSwapiPageGenerator('starships'),
+  },
+};
+
+const swapi = {
+  getPeople: () => swapiEntities.people,
+  getPlanets: () => swapiEntities.planets,
+  getFilms: () => swapiEntities.films,
+  getSpecies: () => swapiEntities.species,
+  getVehicles: () => swapiEntities.vehicles,
+  getStarships: () => swapiEntities.starships,
+};
+
+// log all swapi just for testing
+for (const entity of Object.keys(swapiEntities)) {
+  for await (const page of swapiEntities[entity]) {
+    console.log(page);
+  }
+}
+
+export default swapi;
